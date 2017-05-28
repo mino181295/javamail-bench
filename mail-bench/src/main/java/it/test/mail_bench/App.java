c816ad1378e2843fc71a60f7d95d89b8025d8bc8
Matteo Minardi <matteo.minardi@studio.unibo.it>
2017-05-28 17:56:36 +0200

    Fix

--- mail-bench/src/main/java/it/test/mail_bench/App.java
+++ mail-bench/src/main/java/it/test/mail_bench/App.java
@@ -6,6 +6,6 @@ import it.test.view.BenchmarkView;
 
 public class App {
 	public static void main( String[] args ) {			
-		new Controller(new BenchmarkView(), new BenchmarkModel()).start();	
+		new Controller(new BenchmarkView(), new BenchmarkModel()).start();			
 	}
 }
